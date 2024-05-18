d16821a3de38d4a7fc8f73b090ae946e49e470a1
sorinandreienache <sorin.enache@edu.unifi.it>
2024-05-18 16:18:37 +0200

    Sviluppo dei principali metodi della classe nota

--- Note.cpp
+++ Note.cpp
@@ -3,12 +3,28 @@
 //
 
 #include "Note.h"
-#include "iostream"
+#include <iostream>
 
 using namespace std;
 
-Note::Note(std::string Title , std::string Text) : Title(Title), Text(Text), Lock(false) {}
+Note::Note(std::string Title, std::string Text) : Title(Title), Text(Text), Lock(false) {}
 
 bool Note::LockNote(const Note &note) {
     return Lock = true;
-};
+}
+
+bool Note::UnLockNote(const Note &note) {
+    return Lock = false;
+}
+
+bool Note::StatusNote(const Note &note) const {
+    return Lock;
+}
+
+void Note::modifyText(Note &note, std::string newText) const {
+    if (!StatusNote(note))
+        note.Text = newText;
+    else {
+        std::cout << "Cannot modify a locked note." << std::endl;
+    }
+}
--- Note.h
+++ Note.h
@@ -4,19 +4,34 @@
 
 #ifndef ENACHE_SORIN_PROJECT_NOTE_H
 #define ENACHE_SORIN_PROJECT_NOTE_H
-#endif //ENACHE_SORIN_PROJECT_NOTE_H
-#include "iostream"
+#endif  //ENACHE_SORIN_PROJECT_NOTE_H
+#include <iostream>
 
 using namespace std;
 
 class Note {
 public:
-    Note(std::string Title , std::string Text);
+    Note(std::string Title, std::string Text);
 
     bool LockNote(const Note &note);
 
+    bool UnLockNote(const Note &note);
+
+    bool StatusNote(const Note &note) const;
+
+    std::string getTitle() const {
+        return Title;
+    };
+
+    std::string getText() const {
+        return Text;
+    }
+
+    void modifyText(Note &note, std::string) const;
+
+
 private:
-    string Title;
-    string Text;
+    std::string Title;
+    std::string Text;
     bool Lock;
 };
