513d3052fbb7943ca2f9bfd0e9332f1152133ecb
sorinandreienache <sorin.enache@edu.unifi.it>
2024-05-23 16:10:22 +0200

    prove sul main

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include <ncurses.h>
 
+#include "Collection.h"
 //Classi che rappresentano note (es. Bear / Evernote). Ogni nota è composta da titolo e testo, può essere bloccata per evitarne
 //cancellazione/modifica e fa parte di una collezione specificata con nome.
 //Può anche far parte di una collezione di note importanti oltre che di una collezione specifica.
@@ -7,6 +9,11 @@
 //Reformat Code  press ⌘Сmd ⌥Opt L.
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Note *nota = new Note("title" , "testo");
+    std::cout << nota->getText();
+    nota->LockNote();
+    nota->UnLockNote();
+    std::cout << nota->StatusNote();
+
     return 0;
 }
